0cba3f0d9b28dbcda03dd9946a4290a5305ebbba
Christopher Trinh <Chris.trinh@live.com>
2020-08-13 12:48:58 -0400

    updated add.js and index.js

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
-const layout = require("./views/layout");
+const layout = require('./views/layout');
 
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/public"));
+//
+const { db, Page, User } = require('./models');
 
-app.get("/", (req, res) => {
-  res.send(layout());
+db.authenticate().then(() => {
+	console.log('connected to the database');
 });
 
-app.listen(
-  3000,
-  console.log("listening on port 3000 at http://127.0.0.1:3000/")
-);
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.send(layout());
+});
+
+const init = async () => {
+	await db.sync({ force: true });
+	// await Page.sync();
+	// await User.sync();
+
+	app.listen(
+		3000,
+		console.log('listening on port 3000 at http://127.0.0.1:3000/')
+	);
+};
+
+//invoke init
+init();
